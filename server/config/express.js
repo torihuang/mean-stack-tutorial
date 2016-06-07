94a7aa9c8f83725c575a5de38ffb41bdf753aff2
torihuang <victoriahuang20@gmail.com>
2016-06-07 14:28:02 -0500

    Complete server side login

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,10 @@
 var express = require('express'),
   stylus = require('stylus'),
   logger = require('morgan'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session')
+  passport = require('passport');
 
 module.exports = function (app, config) {
   function compile(str, path) {
@@ -12,9 +15,12 @@ module.exports = function (app, config) {
   app.set('view engine', 'jade');
 
   app.use(logger('dev'));
+  app.use(cookieParser());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-
+  app.use(session({secret: 'multi vision unicorns', resave:false, saveUninitialized:false}));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(stylus.middleware(
     {
       src: config.rootPath + '/public',
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,27 @@
+var passport = require('passport');
 
 module.exports = function(app) {
   app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params[0]);
+  })
+
+  // Using passport in an unusual way, because we are not logging in with server side route
+  // we are logging in using an xhr post
+  app.post('/login', function(req, res, next) {
+    // Create auth function
+    var auth = passport.authenticate('local', function(err, user) {
+      if(err) {return next(err);}
+      if(!user) { res.send({success:false})}
+
+      // Won't need this if using passport as is typical
+      req.logIn(user, function(err) {
+        if(err) {return next(err);}
+        res.send({success: true, user: user})
+      })
+    })
+
+    // Call auth function
+    auth(req, res, next);
   })
 
   // ALL requests go through star route
