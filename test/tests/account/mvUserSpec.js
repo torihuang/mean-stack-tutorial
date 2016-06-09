0b7d1250341b058d56fbcc156a212e9c9a99e5cf
torihuang <victoriahuang20@gmail.com>
2016-06-09 14:15:26 -0500

    Add basic unit test

---
+++ test/tests/account/mvUserSpec.js
@@ -0,0 +1,11 @@
+describe('mvUser', function() {
+  beforeEach(module('app'));
+
+  describe('isAdmin', function() {
+    it('should return false if the roles array does not have an admin entry', inject(function(mvUser) {
+      var user = new mvUser();
+      user.roles = ['not admin'];
+      expect(user.isAdmin()).to.be.falsey;
+    }))
+  })
+})
